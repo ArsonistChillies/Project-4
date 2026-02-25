42b0fbe5aa799af47278c265600ce8ea199c18b8
Kyran Detamore <shimmershine199@gmail.com>
2026-02-25 14:21:36 -0800

    Update App.js

--- client/src/App.js
+++ client/src/App.js
@@ -1,15 +1,58 @@
-import logo from './logo.svg';
-import './App.css';
-import 'bootstrap/dist/css/bootstrap.min.css';
+import { BrowserRouter, Routes, Route, Navigate } from "react-router-dom";
 
+import Navbar from "./components/Navbar";
+import Login from "./Pages/Login";
+import Dashboard from "./Pages/Dashboard";
+import Ask from "./pages/Ask";
+import Category from "./Pages/Category";
+import Register from './Pages/Register'
+function App(){
 
-function App() {
-  return (
-    <>
-      <h2>React</h2>
+const user = localStorage.getItem("user");
 
-    </>
-  );
+return(
+
+<BrowserRouter>
+
+{/* NAVBAR RESTORED */}
+<Navbar/>
+
+<Routes>
+
+<Route 
+path="/" 
+element={user ? <Navigate to="/dashboard"/> : <Login/>}
+/>
+
+<Route path="/register" element={<Register/>}/>
+
+<Route 
+path="/dashboard" 
+element={
+<Dashboard/>
+}
+/>
+
+<Route 
+path="/ask" 
+element={
+<Ask/>
+}
+/>
+
+<Route 
+path="/category/:id" 
+element={
+<Category/>
+}
+/>
+
+</Routes>
+
+</BrowserRouter>
+
+);
+
 }
 
 export default App;
