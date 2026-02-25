3e898f1ff46a7b76912ef20d31d1926fa73abc82
Kyran Detamore <shimmershine199@gmail.com>
2026-02-25 14:03:47 -0800

    Create CategoriesRouter.js

---
+++ server/Routers/CategoriesRouter.js
@@ -0,0 +1,25 @@
+import express from "express";
+import db from "../dataDBConnections.js";
+
+const router = express.Router();
+
+router.get("/", async(req,res)=>{
+
+try{
+
+const [rows] = await db.query(
+"SELECT id,name,description FROM categories ORDER BY name"
+);
+
+res.json(rows);
+
+}catch(err){
+
+console.log(err);
+res.status(500).send("Failed");
+
+}
+
+});
+
+export default router;
