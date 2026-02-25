261023b7616a32afe4006b07807b1c06f5c003c4
Kyran Detamore <shimmershine199@gmail.com>
2026-02-25 14:03:33 -0800

    Create Navbar.jsx

---
+++ client/src/components/Navbar.jsx
@@ -0,0 +1,40 @@
+import { Link, useNavigate } from 'react-router-dom';
+
+export default function Navbar() {
+        const nav=useNavigate();
+        const user=localStorage.getItem("user");
+
+        if(!user) return null;
+
+        function logout(){
+        localStorage.removeItem("user");
+        nav("/");
+    }
+
+    return(
+        <nav className="navbar navbar-dark bg-dark navbar-expand-lg">
+
+            <div className="container">
+
+                <Link className="navbar-brand fw-bold" to="/dashboard">
+                
+                </Link>
+
+                <div>
+
+                    <Link className="btn btn-outline-light me-2" to="/dashboard">
+                    Home
+                    </Link>
+
+                    <button onClick={logout} className="btn btn-danger">
+                    Logout
+                    </button>
+
+                </div>
+
+            </div>
+
+        </nav>
+
+    );
+}
