3cc514320689e4456c1c4a9633dd0420172e3c48
Kyran Detamore <shimmershine199@gmail.com>
2026-02-25 19:09:05 -0800

    Update index.js

--- server/index.js
+++ server/index.js
@@ -1,12 +1,14 @@
 import express from 'express'
 import cors from 'cors'
 import yourRouter from './Routers/yourRouter.js'
+import authRouter from './Routers/AuthRouter.js'
  
 const server = express();
  
 server.use(express.json())
 server.use(cors())
 server.use("/abc", yourRouter)
+server.use("/auth", authRouter)
  
 server.get("/", (req, res) => {
  res.send("The server is running")
