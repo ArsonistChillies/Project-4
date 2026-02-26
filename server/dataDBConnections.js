424266f0f2c495d8d9cc6ee5dd70f0544cc265b2
Kyran Detamore <shimmershine199@gmail.com>
2026-02-25 17:15:54 -0800

    Update dataDBConnections.js

--- server/dataDBConnections.js
+++ server/dataDBConnections.js
@@ -1,18 +1,13 @@
-import mysql from 'mysql2/promise'
- 
-let db
- 
-try {
- db = await mysql.createConnection ( {
- host: 'localhost',
- user: 'root',
- password: 'Ss04212006.',
- database: 'localhost:3306/stack_overflow_clone'
- })
- console.log ("Connected to database")
-}
-catch (error) {
- console.log ("error connecting to database: ", error)
-}
- 
+import mysql from "mysql2/promise";
+import dotenv from "dotenv";
+
+dotenv.config();
+
+const db = await mysql.createConnection({
+  host: process.env.DB_HOST,
+  user: process.env.DB_USER,
+  password: process.env.DB_PASSWORD,
+  database: process.env.DB_NAME
+});
+
 export default db;
